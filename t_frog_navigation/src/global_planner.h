81bfb06a32fecc7e923698944f00614bc3b8c7b9
DaikiMaekawa <method_aspect_card@yahoo.co.jp>
2014-09-30 18:58:48 +0900

    Provisional Implementation of the simple planner

---
+++ t_frog_navigation/src/global_planner.h
@@ -0,0 +1,30 @@
+#include <ros/ros.h>
+#include <costmap_2d/costmap_2d_ros.h>
+#include <costmap_2d/costmap_2d.h> 
+#include <nav_core/base_global_planner.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <angles/angles.h>
+#include <base_local_planner/world_model.h>
+#include <base_local_planner/costmap_model.h>
+
+using std::string;
+
+#ifndef GLOBAL_PLANNER_CPP
+#define GLOBAL_PLANNER_CPP
+
+namespace global_planner {
+
+    class GlobalPlanner : public nav_core::BaseGlobalPlanner {
+    public:
+        GlobalPlanner();
+        GlobalPlanner(std::string name, costmap_2d::Costmap2DROS *costmap_ros);
+
+        void initialize(std::string name, costmap_2d::Costmap2DROS *costmap_ros);
+        bool makePlan(const geometry_msgs::PoseStamped &start,
+                      const geometry_msgs::PoseStamped &goal,
+                      std::vector<geometry_msgs::PoseStamped> &plan);
+    };
+};
+
+#endif
+
