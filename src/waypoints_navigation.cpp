174462817a96f70804991f75cd28a2480107b8c1
DaikiMaekawa <method_aspect_card@yahoo.co.jp>
2014-10-10 15:53:42 +0900

    Add the function of the NavigationGL

--- src/waypoints_navigation.cpp
+++ src/waypoints_navigation.cpp
@@ -1,6 +1,12 @@
 #include <ros/ros.h>
-#include <yaml-cpp/yaml.h>
 #include <geometry_msgs/PointStamped.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+#include <tf/tf.h>
+#include <tf/transform_listener.h>
+
+#include <yaml-cpp/yaml.h>
 
 #include <vector>
 #include <fstream>
@@ -8,12 +14,23 @@
 
 class WaypointsNavigation{
 public:
-    WaypointsNavigation(){
+    WaypointsNavigation() :
+        has_activate_(false),
+        move_base_action_("move_base", true)
+    {
+        while((move_base_action_.waitForServer(ros::Duration(1.0)) == false) && (ros::ok() == true))
+        {
+            ROS_INFO("Waiting...");
+        }
+        
+        
         ros::NodeHandle private_nh("~");
+        private_nh.param("robot_frame", robot_frame_, std::string("/base_link"));
+        private_nh.param("world_frame", world_frame_, std::string("/map"));
         std::string filename = "";
         private_nh.param<std::string>("filename", filename, filename);
         if(filename != ""){
-            ROS_INFO_STREAM("Read waypoints from " << filename);
+            ROS_INFO_STREAM("Read waypoints data from " << filename);
             readFile(filename);
         }
         for(int i=0; i < waypoints_.size(); i++){
@@ -44,13 +61,24 @@ public:
                     waypoints_.push_back(point);
 
                 }
+            }else{
+                return false;
             }
 
             const YAML::Node &fp_node_tmp = node["finish_pose"];
             const YAML::Node *fp_node = fp_node_tmp ? &fp_node_tmp : NULL;
 
             if(fp_node != NULL){
-                //TODO
+                finish_pose_.pose.position.x = (*fp_node)["pose"]["position"]["x"].as<double>();
+                finish_pose_.pose.position.y = (*fp_node)["pose"]["position"]["y"].as<double>();
+                finish_pose_.pose.position.z = (*fp_node)["pose"]["position"]["z"].as<double>();
+
+                finish_pose_.pose.orientation.x = (*fp_node)["pose"]["orientation"]["x"].as<double>();
+                finish_pose_.pose.orientation.y = (*fp_node)["pose"]["orientation"]["y"].as<double>();
+                finish_pose_.pose.orientation.z = (*fp_node)["pose"]["orientation"]["z"].as<double>();
+                finish_pose_.pose.orientation.w = (*fp_node)["pose"]["orientation"]["w"].as<double>();
+            }else{
+                return false;
             }
 
         }catch(YAML::ParserException &e){
@@ -60,17 +88,84 @@ public:
             return false;
         }
 
+        has_activate_ = true;
         return true;
     }
 
-private:
-    std::vector<geometry_msgs::PointStamped> waypoints_;
+    bool shouldSendGoal(){
+        bool ret = true;
+        actionlib::SimpleClientGoalState state = move_base_action_.getState();
+        if((state != actionlib::SimpleClientGoalState::ACTIVE) &&
+           (state != actionlib::SimpleClientGoalState::PENDING) && 
+           (state != actionlib::SimpleClientGoalState::RECALLED) &&
+           (state != actionlib::SimpleClientGoalState::PREEMPTED))
+        {
+            ret = false;
+        }
 
+        if(waypoints_.empty()){
+            ret = false;
+        }
+
+        return ret;
+    }
+
+    bool navigationFinished(const geometry_msgs::Point &dest, double dist_err = 0.5){
+        tf::StampedTransform robot_gl;
+        try{
+            tf_listener_.lookupTransform(world_frame_, robot_frame_, ros::Time(0.0), robot_gl);
+        }catch(tf::TransformException &e){
+            ROS_WARN_STREAM("tf::TransformException: " << e.what());
+        }
+        const double wx = dest.x;
+        const double wy = dest.y;
+        const double rx = robot_gl.getOrigin().x();
+        const double ry = robot_gl.getOrigin().y();
+        const double dist = std::sqrt(std::pow(wx - rx, 2) + std::pow(wy - ry, 2));
+
+        return dist < dist_err;
+    }
+
+    void startNavigationGL(const geometry_msgs::Point &dest){
+        move_base_msgs::MoveBaseGoal move_base_goal;
+        move_base_goal.target_pose.header.stamp = ros::Time::now();
+        move_base_goal.target_pose.header.frame_id = world_frame_;
+        move_base_goal.target_pose.pose.position = dest;
+        move_base_goal.target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(0.0);
+        
+        move_base_action_.sendGoal(move_base_goal);
+    }
+
+    void run(){
+        ros::Rate rate(10.0);
+        while(ros::ok()){
+            for(int i=0; i < waypoints_.size(); i++){
+                if(!ros::ok()) break;
+                
+                startNavigationGL(waypoints_[i].point);
+                while(!navigationFinished(waypoints_[i].point) && ros::ok()){
+                    ros::spinOnce();
+                    rate.sleep();
+                }
+            }
+            ros::spinOnce();
+            rate.sleep();
+        }
+    }
+
+private:
+    actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> move_base_action_;
+    std::vector<geometry_msgs::PointStamped> waypoints_;
+    geometry_msgs::PoseStamped finish_pose_;
+    bool has_activate_;
+    std::string robot_frame_, world_frame_;
+    tf::TransformListener tf_listener_;
 };
 
 int main(int argc, char *argv[]){
     ros::init(argc, argv, ROS_PACKAGE_NAME);
     WaypointsNavigation w_nav;
+    w_nav.run();
 
     return 0;
 }
