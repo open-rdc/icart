f5d7c498ae13ed684614e1ac8859ae8bc7cbcdca
DaikiMaekawa <method_aspect_card@yahoo.co.jp>
2014-12-16 20:58:20 +0900

    Add cmake module for YPSpur Library

---
+++ icart_mini_driver/cmake/Modules/FindYPSpur.cmake
@@ -0,0 +1,9 @@
+if ("${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
+# pass
+endif ("${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
+find_path(YPSPUR_INCLUDE_DIR ypspur.h
+HINTS /usr/local/include/ /usr/include/ /opt/local/include)
+find_path(YPSPUR_LIB_DIR ypspur
+HINTS /usr/local/lib /usr/lib /opt/local/lib)
+find_library(YPSPUR_LIBRARY ypspur
+HINTS /usr/local/lib /usr/lib /opt/local/lib)
